d409f292245c1d855e4ab8e14fbdac673c4ccab4
wiss-ali <wiss.dev@outlook.fr>
2024-01-17 20:50:49 +0100

    jh

--- page-profil.php
+++ page-profil.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['User_id'])) {
     header('Location: page-connexion.php');
     exit();
 }
