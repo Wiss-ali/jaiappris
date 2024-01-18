c5d10faa29705cf16d4ebea743d52c7b7cb041e6
wiss-ali <wiss.dev@outlook.fr>
2024-01-18 17:13:28 +0100

    k,

--- sup-publication.php
+++ sup-publication.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once "config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id']) && isset($_SESSION['Users_id'])) {
