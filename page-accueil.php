e266454b1e3aa6ffd7af28203292791628be0847
wiss-ali <wiss.dev@outlook.fr>
2024-01-18 05:04:49 +0100

    vv

--- page-accueil.php
+++ page-accueil.php
@@ -56,6 +56,7 @@ $mysqli->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page d'accueil</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <h1>Posts récents</h1>
